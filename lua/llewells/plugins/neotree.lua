91e803d05b4f0dd7088dc2aafe924cbca8dda892
Simon Llewellyn <llewells86@gmail.com>
2025-07-19 12:26:19 +0100

    added autoclose of neotree when opening file

--- lua/llewells/plugins/neotree.lua
+++ lua/llewells/plugins/neotree.lua
@@ -7,4 +7,16 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				event = "file_open_requested",
+				handler = function()
+					require("neo-tree.command").execute({
+						action = "close",
+					})
+				end,
+			},
+		})
+	end,
 }
