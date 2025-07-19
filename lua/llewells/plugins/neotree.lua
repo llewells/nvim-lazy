32b7bd63db95380b8350d8992c1166c784c3d01b
Simon Llewellyn <llewells86@gmail.com>
2025-07-19 13:04:21 +0100

    added config but position not working

--- lua/llewells/plugins/neotree.lua
+++ lua/llewells/plugins/neotree.lua
@@ -1,3 +1,12 @@
+vim.cmd([[
+    highlight! link NeoTreeDirectoryIcon NvimTreeFolderIcon
+    highlight! link NeoTreeDirectoryName NvimTreeFolderName
+    highlight! link NeoTreeSymbolicLinkTarget NvimTreeSymlink
+    highlight! link NeoTreeRootName NvimTreeRootFolder
+    highlight! link NeoTreeDirectoryName NvimTreeOpenedFolderName
+    highlight! link NeoTreeFileNameOpened NvimTreeOpenedFile
+]])
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
 	branch = "v3.x",
@@ -7,15 +16,49 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	lazy = false,
 	config = function()
 		require("neo-tree").setup({
-			event_handlers = {
-				event = "file_open",
-				handler = function()
-					require("neo-tree.command").execute({
-						action = "close",
-					})
-				end,
+			filesystem = {
+				components = {
+					harpoon_index = function(config, node, _)
+						local Marked = require("harpoon.mark")
+						local path = node:get_id()
+						local success, index = pcall(Marked.get_index_of, path)
+						if success and index and index > 0 then
+							return {
+								text = string.format("%d ", index), -- <-- Add your favorite harpoon like arrow here
+								highlight = config.highlight or "NeoTreeDirectoryIcon",
+							}
+						else
+							return {
+								text = "  ",
+							}
+						end
+					end,
+				},
+				renderers = {
+					file = {
+						{ "icon" },
+						{ "name", use_git_status_colors = true },
+						{ "harpoon_index" }, --> This is what actually adds the component in where you want it
+						{ "diagnostics" },
+						{ "git_status", highlight = "NeoTreeDimText" },
+					},
+				},
+			},
+			window = {
+				popup = {
+					position = { col = "100%", row = "2" },
+					size = function(state)
+						local root_name = vim.fn.fnamemodify(state.path, ":~")
+						local root_len = string.len(root_name) + 4
+						return {
+							width = math.max(root_len, 50),
+							height = vim.o.lines - 6,
+						}
+					end,
+				},
 			},
 		})
 	end,
